daf06f12a64098ee86f057a93591663314406e22
rg27 <gualvezreymar@gmail.com>
2020-12-29 07:59:07 +0800

    Adding the Postjs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
-import { Admin, Resource } from 'react-admin';
-import { UserList } from './users';
+import { Admin, Resource, EditGuesser   } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from './users';
+import { PostList } from './posts';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
  const App = () => (
     <Admin dataProvider={dataProvider}>
+      <Resource name="posts" list={PostList} edit={EditGuesser} />
       <Resource name="users" list={UserList} />
     </Admin>
 )
