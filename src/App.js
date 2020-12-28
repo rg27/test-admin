8d378adc422f14e106bc0a6e875d6b02e5629203
rg27 <gualvezreymar@gmail.com>
2020-12-28 18:15:22 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Admin, Resource } from 'react-admin';
+import { UserList } from './users';
+import jsonServerProvider from 'ra-data-json-server';
 
-export default App;
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+ const App = () => (
+    <Admin dataProvider={dataProvider}>
+      <Resource name="users" list={UserList} />
+    </Admin>
+)
+
+export default App
